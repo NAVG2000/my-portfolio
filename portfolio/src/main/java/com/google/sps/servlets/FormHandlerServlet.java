d1ac32fde4871b9fb44547ecacc5ace143067037
NAVG2000 <navg2000@gmail.com>
2021-03-14 16:34:45 +0000

    Implement sentiment analysis on message

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -6,6 +6,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+
+import com.google.gson.Gson;
+
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
 
@@ -18,17 +24,26 @@ public class FormHandlerServlet extends HttpServlet {
     String summary = request.getParameter("summary");
     String message = request.getParameter("message");
 
+    // sentiment analysis
+    Document doc = Document.newBuilder()
+        .setContent(request.getParameter("message"))
+        .setType(Document.Type.PLAIN_TEXT).build();
+    LanguageServiceClient languageService = LanguageServiceClient.create();
+    Sentiment sentiment = languageService.analyzeSentiment(doc).getDocumentSentiment();
+    float score = sentiment.getScore();
+    languageService.close();
+
     // Print the value so you can see it in the server logs.
     System.out.println("Message from: " + name);
     System.out.println("Is a recruiter: " + recruiter);
     System.out.println("Summary of Message: " + summary);
     System.out.println("Message: " + message);
+    System.out.println("Sentiment Score: "+ score);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("Message from: " + name);
-    response.getWriter().println("Is a recruiter: " + recruiter);
-    response.getWriter().println("Summary of Message: " + summary);
-    response.getWriter().println("Message: " + message);
-    response.sendRedirect(request.getContextPath());
+    Gson gson = new Gson();
+    String score_response = gson.toJson(score);
+    response.setContentType("application/json;");
+    response.getWriter().println(score_response);
   }
 }
